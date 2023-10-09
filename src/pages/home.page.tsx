84a65c1e37505563020657e323816e39d364ba83
Samuelplim <samuel12nerd@gmail.com>
2023-10-08 22:05:00 -0400

    updated

--- src/pages/home.page.tsx
+++ src/pages/home.page.tsx
@@ -1,5 +1,10 @@
 import { useState, useCallback, useEffect } from "react";
-import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  useJsApiLoader,
+  Marker,
+  GoogleMapProps,
+} from "@react-google-maps/api";
 import { fethEvents } from "../services/eonet.services";
 import { Event } from "../interfaces";
 import CustomMarker from "../assets/images/wildfires.png";
@@ -19,20 +24,21 @@ export const HomePage = () => {
     lat: 33.2128,
     lng: -100.2617235,
   };
-  const [map, setMap] = useState(null);
+  // eslint-disable-next-line
+  const [map, setMap] = useState<google.maps.Map | null>(null);
   const { isLoaded } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: import.meta.env.VITE_API_KEY_GOOGLE,
   });
-  const onLoad = useCallback(function callback(map) {
+  const onLoad = useCallback(function callback(map: google.maps.Map) {
     // This is just an example of getting and using the map instance!!! don't just blindly copy!
     const bounds = new window.google.maps.LatLngBounds(center);
     map.fitBounds(bounds);
-
+    console.log(map);
     setMap(map);
   }, []);
 
-  const onUnmount = useCallback(function callback(map) {
+  const onUnmount = useCallback(function callback() {
     setMap(null);
   }, []);
 
@@ -45,7 +51,7 @@ export const HomePage = () => {
   }, []);
 
   const _renderMarkers = (eventMarker: Event) => {
-    console.log(eventMarker);
+    //console.log(eventMarker);
     const eventPosition: { lat: number; lng: number } = {
       lat: eventMarker.geometry[0].coordinates[1],
       lng: eventMarker.geometry[0].coordinates[0],
